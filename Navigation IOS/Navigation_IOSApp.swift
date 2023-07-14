a41f1a32c6829cd78694287b9677fc1cc316f941
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2023-07-14 16:42:30 +0600

    Initial Commit

---
+++ Navigation IOS/Navigation_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_IOSApp.swift
+//  Navigation IOS
+//
+//  Created by Rezaul Islam on 14/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
